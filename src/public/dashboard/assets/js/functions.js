797fb178c77bc5d2f6262d53a6e24098544af3ef
= <gabrielvallejo2000@gmail.com>
2023-09-27 12:59:42 -0500

    CRUD CATEGORIES

--- src/public/dashboard/assets/js/functions.js
+++ src/public/dashboard/assets/js/functions.js
@@ -1,4 +1,6 @@
 function cargarTabla(){
+    $('#lightgallery').html('');
+    $('#tabla').DataTable().clear().destroy();
     $('#tabla').DataTable({
         order: [[0, 'desc']], // 0 es el índice de la primera columna
         "processing": true,
@@ -13,7 +15,8 @@ function cargarTabla(){
                 if(data == null){
                     data = 'default.webp';
                 }
-                return `<td><img src="/images/categories/${data}" alt="" class="cart-img"></td>`;
+                
+            return `<img data-imggallery="/images/categories/${data}" class="img-responsive br-5" src="/images/categories/${data}" alt="Thumb-1">`;
             }},
             {"data": "name"},
             {"data": "status", "render": function(data, type, row) {
@@ -25,18 +28,48 @@ function cargarTabla(){
             }},
             {"data": "id", "render": function(data, type, row) {
                 let td = `<a href="javascript:void(0)" id="eliminar" class="btn btn-square btn-danger-light me-1" data-bs-toggle="tooltip" data-bs-placement="top" data-id="${data}" title="" data-bs-original-title="Remove" aria-label="Remove"><i class="icon icon-trash  fs-13"></i></a>`;
-                if(row.status === 1){
+                if(row.status === 0){
                     td = `<a href="javascript:void(0)" id="restaurar" class="btn btn-square btn-success-light me-1" data-bs-toggle="tooltip" data-bs-placement="top" data-id="${data}" title="" data-bs-original-title="Remove" aria-label="Remove"><i class="icon icon-reload  fs-13"></i></a>`;
                 }
                 return `<td>
-                            <a href="javascript:void(0)" id="editar" data-name="${row.name}" data-image="/images/categories/${row.image}" class="btn btn-square btn-primary-light me-1" data-bs-toggle="tooltip" data-bs-placement="top" data-id="${data}" title="" data-bs-original-title="Editar" aria-label="Editar"><i class="icon icon-pencil  fs-13"></i></a>
+                            <a href="javascript:void(0)" id="editar" data-name="${row.name}" data-image="/images/categories/${row.image}" class="btn btn-square btn-primary-light me-1" data-bs-toggle="tooltip" data-bs-placement="top" data-id="${row.id}" title="" data-bs-original-title="Editar" aria-label="Editar"><i class="icon icon-pencil  fs-13"></i></a>
                             ${td}
                             
                         </td>`;
             }},
-        ]
+        ],
+        "drawCallback": function() {
+            var api = this.api();
+            api.rows().every(function(){
+                var data = this.data();
+                var name = data.name;
+                var image = data.image || 'default.webp';
+                $('#lightgallery').append(`<li data-responsive="/images/categories/${image}" data-src="/images/categories/${image}" data-sub-html="<h4>${name}</h4><p> Categoria</p>">
+                        <a href="">
+                        <img class="img-responsive br-5" src="/images/categories/${image}" alt="${name}">
+                        </a>
+                    </li>`);
+            });
+            lightGallery(document.getElementById('lightgallery'));
+        }
     });
 };
+
+function limpiar_inputs(){
+    $('#cancelar_editar').addClass('d-none');
+    $('input').val('');
+    // Obtener la instancia de Dropify
+    let drInstance = $('input[name="image"]').dropify();
+    drInstance = drInstance.data('dropify');
+    image = null;
+    // Restablecer la imagen a su valor original
+    drInstance.clearElement();
+    drInstance.settings.defaultFile = drInstance.getFilePreview();
+    $('#agregar').data('id',null);
+    // Puedes reinicializar la instancia de Dropify si es necesario
+    drInstance.init();
+}
+
 // Función para cargar la vista
 function cargarVista(vista) {
     $.ajax({
@@ -91,3 +124,50 @@ var funcionesVista = {
         
     },
 };
+
+function loaderButton(element){
+    $(element).html(`<i class="fa fa-circle-o-notch fa-spin" data-bs-toggle="tooltip" title="" data-bs-original-title="fa fa-circle-o-notch" aria-label="fa fa-circle-o-notch"></i>`);
+}
+
+/**
+ * Función para hacer una petición AJAX de manera asincrónica.
+ *
+ * @param {string} URL - La URL a la que se hará la petición.
+ * @param {string} TYPE - El tipo de método HTTP (Ejemplos, "GET", "POST","PUT","PATCH","DELETE").
+ * @param {object} FORM_DATA - Datos a enviar con la petición. Por defecto es null.
+ * @returns {Promise} Una promesa que se resuelve con la respuesta de la petición o se rechaza con un error.
+ */
+
+function ajaxForm(URL, TYPE, FORM_DATA = null,updateView = true) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: URL,             // URL a la que se hará la petición.
+            type: TYPE,           // Método HTTP a usar.
+            data: FORM_DATA,      // Datos a enviar con la petición.
+            processData: false,   // Indica a jQuery no procesar los datos.
+            contentType: false,   // No establecer el tipo de contenido, dejar que FormData lo haga.
+            success: function(response) {
+                if(updateView){
+                    cargarTabla();    // Actualiza la tabla con los nuevos datos.
+                    limpiar_inputs(); // Limpia los campos de entrada.   
+                }
+                resolve(response); // Resuelve la promesa con la respuesta obtenida.
+            },
+            error: function(err) {
+                console.error(err); // Muestra el error en la consola.
+                reject(err);        // Rechaza la promesa con el error obtenido.
+            }
+        });
+    });
+}
+
+function cargarDropify(IMAGE){
+     // Destruye y Re-inicializa Dropify con la nueva imagen.
+     let drInstance = $('input[name="image"]').dropify();
+     drInstance = drInstance.data("dropify");
+     drInstance.resetPreview();
+     drInstance.clearElement();
+     drInstance.destroy();
+     drInstance.settings.defaultFile = IMAGE;
+     drInstance.init();
+}
--- src/public/dashboard/assets/js/main.js
+++ src/public/dashboard/assets/js/main.js
@@ -1,75 +1,97 @@
-$(document).ready(function() {
-    // Cargar la vista al recargar la página
-    var vista = obtenerVistaDeURL();
-    cargarVista(vista);
-    
-    // Escuchar clics en los enlaces y cambiar la URL y cargar la vista correspondiente
-    $('a').on('click', function(e) {
-        e.preventDefault();
-        var href = $(this).attr('href');
-        var vista = href.split('/').pop();
-        cambiarURL(href);
-        cargarVista(vista);
-    });
-    
-    // Escuchar cambios en el historial del navegador y cargar la vista correspondiente
-    $(window).on('popstate', function() {
-        var vista = obtenerVistaDeURL();
-        cargarVista(vista);
-    });
-    $(document).on('submit','#form',function(e){
-        e.preventDefault();
-        
-        var formData = new FormData(this);
-        $.ajax({
-            url: '/categories', // URL del archivo que procesará el formulario.
-            type: 'POST',
-            data: formData,
-            processData: false, // Importante: decirle a jQuery no procesar los datos.
-            contentType: false, // Importante: no establecer el tipo de contenido, dejar que FormData lo haga.
-            success: function(response){
-                $('#tabla').DataTable().clear().destroy();
-                cargarTabla();
-            },
-            error: function(err){
-              console.error(err); // Manejar los errores.
-            }
-          });
-      });
+$(document).ready(function () {
+  // Cargar la vista al recargar la página
+  var vista = obtenerVistaDeURL();
+  cargarVista(vista);
 
-      $(document).on('click','#eliminar,#restaurar', function(e){
-        e.preventDefault(); // Prevenir la acción predeterminada del enlace
-        let id = $(this).data('id');
-        $.ajax({
-            url: '/categories/status/' + id, // URL del archivo que procesará el formulario.
-            type: 'PATCH',
-            success: function(response){
-              let button = `<a href="javascript:void(0)" id="restaurar" class="btn btn-square btn-success-light me-1" data-bs-toggle="tooltip" data-bs-placement="top" data-id="${id}" title="" data-bs-original-title="Remove" aria-label="Remove"><i class="icon icon-reload  fs-13"></i></a>`;
-              if(response.data === 0){
-                button = `<a href="javascript:void(0)" id="eliminar" class="btn btn-square btn-danger-light me-1" data-bs-toggle="tooltip" data-bs-placement="top" data-id="${id}" title="" data-bs-original-title="Remove" aria-label="Remove"><i class="icon icon-trash  fs-13"></i></a>`
-              }
-                $(e.target).replaceWith(button);
-            },
-            error: function(err){
-              console.error(err); // Manejar los errores.
-            }
-          });
+  // Escuchar clics en los enlaces y cambiar la URL y cargar la vista correspondiente
+  $("a").on("click", function (e) {
+    e.preventDefault(); // Evitar el comportamiento predeterminado del enlace en todos los casos.
+
+    var href = $(this).attr("href");
+
+    if (href === "#") {
+      return false; // Finaliza la función si el href es igual a "#".
+    }
+
+    var vista = href.split("/").pop();
+    cambiarURL(href);
+    cargarVista(vista);
+  });
+
+  // Escuchar cambios en el historial del navegador y cargar la vista correspondiente
+  $(window).on("popstate", function () {
+    if (!window.location.hash) {
+      var vista = obtenerVistaDeURL();
+      cargarVista(vista);
+    }
+  });
+
+  // Listener para mostrar la galería al hacer click a cualquier imagen de la tabla
+  $(document).on("click", "[data-imgGallery]", function () {
+    let img = $(this).data("imggallery");
+    $(`#lightgallery [data-src="${img}"]`).trigger("click");
+  });
+
+  $(document).on("submit", "#form", function (e) {
+    e.preventDefault();
+    let id = $("#agregar").data("id");
+    // Para crear un nuevo registro
+    let API = {
+      url: "/categories",
+      type: "POST",
+    };
+    // Para editar un regristro
+    if (id > 0) {
+      API = {
+        url: "/categories/" + id,
+        type: "PUT",
+      };
+    }
+    // Tomar datos del formulario
+    var formData = new FormData(this);
+    ajaxForm(API.url, API.type, formData)
+      .then((response) => {
+        id = 0;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
       });
-      let image = null;
-      $(document).on('click','#editar', function(){
-        let name = $(this).data('name');
-        let image = $(this).data('image');
-        
-        $('input[name="name"]').val(name);
-        
-        // Destruye y Re-inicializa Dropify con la nueva imagen.
-        let drInstance = $('input[name="image"]').dropify();
-        drInstance = drInstance.data('dropify');
-        drInstance.resetPreview();
-        drInstance.clearElement();
-        drInstance.destroy();
-        drInstance.settings.defaultFile = image;
-        drInstance.init();
-    });
-    
+  });
+
+  $(document).on("click", "#eliminar,#restaurar", function (e) {
+    let id = $(this).data("id");
+    let button_element = e.target;
+    loaderButton(button_element);
+    ajaxForm("/categories/status/" + id, 'PATCH',null,false)
+      .then((response) => {
+        let tag = `<span class="badge bg-danger">Desactivada</span>`;
+        let button = `<a href="javascript:void(0)" id="restaurar" class="btn btn-square btn-success-light me-1" data-bs-toggle="tooltip" data-bs-placement="top" data-id="${id}" title="" data-bs-original-title="Remove" aria-label="Remove"><i class="icon icon-reload  fs-13"></i></a>`;
+        if (response.data == 1) {
+          tag = `<span class="badge bg-success">Disponible</span>`;
+          button = `<a href="javascript:void(0)" id="eliminar" class="btn btn-square btn-danger-light me-1" data-bs-toggle="tooltip" data-bs-placement="top" data-id="${id}" title="" data-bs-original-title="Remove" aria-label="Remove"><i class="icon icon-trash  fs-13"></i></a>`;
+        }
+        $(button_element).parent().prev().html(tag);
+        $(button_element).replaceWith(button);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+  let image = null;
+  $(document).on("click", "#editar", function () {
+    $('#cancelar_editar').removeClass('d-none');
+    let name = $(this).data("name");
+    let image = $(this).data("image");
+    let id = $(this).data("id");
+    $("#agregar").data("id", id);
+    $('input[name="name"]').val(name);
+    cargarDropify(image);
+    $('#agregar').addClass('btn-primary').removeClass('success').html('Editar');
+  });
+
+  $(document).on("click", "#cancelar_editar", function () {
+    limpiar_inputs();
+    $('#agregar').addClass('btn-success').removeClass('primary').html('Agregar');
+  });
+
 });
